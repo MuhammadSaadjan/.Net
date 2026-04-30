e9164a5125d996867242acb7935c0e2ecfafcb8d
Muhammad Saad Jan <101183587+MuhammadSaadjan@users.noreply.github.com>
2026-04-30 21:53:06 +0500

    (Guessing Game)Task-2

---
+++ Task2-guessingGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class GuessingGame
+{
+    static void Main() {
+        int coded_num = 15;
+        int Guess = 0;
+        int attempts = 0; 
+        Console.Write("Guess the number: ");
+
+        while (Guess != coded_num) {
+            
+            bool isValid = int.TryParse(Console.ReadLine(), out Guess);
+            if (!isValid)
+            {
+                
+                    Console.Write("Invalid input. Please enter a number: ");
+                    continue;
+                
+            }
+            attempts++;
+            if (Guess < coded_num) {
+                Console.Write("Too Low, Try again.");
+            }
+            if (Guess > coded_num) {
+                Console.Write("Too High, Try Again.");
+            }
+        }
+
+        
+            Console.WriteLine("Congratulations! You Found the Secret number");
+        Console.Write($"You found the number in {attempts} attempts");
+       
+
+    }
+}
